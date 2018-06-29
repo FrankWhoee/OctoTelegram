a75a405298829f0707b940f77866eace30afe7f6
Frank Hui <huifrank@live.com>
2018-06-28 17:35:01 -0700

    Added date joined for User.

--- OctoTelegram/src/main/java/blockchain/socialmedia/octotelegram/User.java
+++ OctoTelegram/src/main/java/blockchain/socialmedia/octotelegram/User.java
@@ -1,17 +1,22 @@
 package blockchain.socialmedia.octotelegram;
 
 import java.util.ArrayList;
+import java.util.Date;
+
 
 public class User {
 	
 	private Long id;
 	private String username;
 	private ArrayList<User> friends;
+	private Date dateJoined;
 	
-	public User(Long id, String username, ArrayList<User> friends) {
+	
+	public User(Long id, String username, ArrayList<User> friends,Date dateJoined) {
 		this.id = id;
 		this.username = username;
 		this.friends = friends;
+		this.dateJoined = dateJoined;
 	}
 	
 	public Long getIdLong() {
