df407dd914e9faecc66ea5da259d027e26c5480d
Andy Shen <andyshenproper@gmail.com>
2018-06-28 17:33:03 -0700

    Auto stash before merge of "master" and "origin/master"

--- OctoTelegram/src/main/java/blockchain/socialmedia/octotelegram/Post.java
+++ OctoTelegram/src/main/java/blockchain/socialmedia/octotelegram/Post.java
@@ -1,5 +1,35 @@
 package blockchain.socialmedia.octotelegram;
 
 public class Post {
+	
+	private String title, content;
+	private int id, timecode; // timecode ddmmyyyyhhmm
+	private User author;
 
+	public Post(String title, String content, int timecode, User author){
+		this.title = title;
+		this.content = content;
+		this.timecode = timecode;
+		this.author = author;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public int getTimecode() {
+		return timecode;
+	}
+
+	public User getAuthor() {
+		return author;
+	}
 }
