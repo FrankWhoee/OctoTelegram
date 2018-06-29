7cbc85cc9123404c411a1eea6d5b1bf2209b770f
Andy Shen <andyshenproper@gmail.com>
2018-06-28 21:57:53 -0700

    Small Changes

--- OctoTelegram/src/main/java/blockchain/socialmedia/octotelegram/App.java
+++ OctoTelegram/src/main/java/blockchain/socialmedia/octotelegram/App.java
@@ -1,10 +1,13 @@
 package blockchain.socialmedia.octotelegram;
 
+import java.util.ArrayList;
+import java.util.Date;
 
 public class App 
 {
     public static void main( String[] args )
     {
+    	//OctoTelegram.PostTelegram("Hi", "testing", new User((long) 104, "Hi", new ArrayList(), new Date()));
         System.out.println(java.time.LocalDate.now());
     }
 }
